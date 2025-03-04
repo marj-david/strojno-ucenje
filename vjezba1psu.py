be941fbb9889bc582d0a723df92accfe2ce0e933
marj-david <168465724+marj-david@users.noreply.github.com>
2025-03-04 13:06:39 +0100

    Update vjezba1psu.py

--- vjezba1psu.py
+++ vjezba1psu.py
@@ -1,3 +1,11 @@
+#Napišite program koji od korisnika zahtijeva unos radnih sati te koliko je plaćen po radnom satu. Koristite ugrađenu
+#Python metodu input(). Nakon toga izračunajte koliko je korisnik zaradio i ispišite na ekran. Na kraju prepravite
+#rješenje na način da ukupni iznos izračunavate u zasebnoj funkciji naziva total_euro.
+#Primjer:
+#Radni sati: 35 h
+#eura/h: 8.5
+#Ukupno: 297.5 eura
+
 print("Unesi radne sate")
 radni_sati = input()
 radni_sati = int(radni_sati)
