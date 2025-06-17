15c4364d3e85650557f926d938697474e7e9fbd7
marj-david <168465724+marj-david@users.noreply.github.com>
2025-06-17 13:16:36 +0200

    Update lv3vjezba1.py

--- lv3/lv3vjezba1.py
+++ lv3/lv3vjezba1.py
@@ -10,22 +10,36 @@
 
 import pandas as pd
 import numpy as np
-mtcars = pd.read_csv('mtcars.csv')
+data = pd.read_csv('mtcars.csv')
 
-print(mtcars.sort_values(by='mpg', ascending=False)[0:5])
-print(mtcars[mtcars.cyl==8], mtcars.sort_values(by="mpg", ascending = True)[0:3])
+# 1. Kojih 5 automobila ima najveću potrošnju? (mpg - miles per gallon, manja vrijednost znači veću potrošnju)
+top5_potrosnja = data.sort_values(by='mpg', ascending=True).head(5)
+print("5 automobila s najvećom potrošnjom:")
+print(top5_potrosnja[['car', 'mpg']])
 
-new_cars = mtcars[mtcars.cyl==6]
-new_cars = new_cars.groupby('mpg')
-print(new_cars['mpg'].mean())
+# 2. Tri automobila s 8 cilindara s najmanjom potrošnjom
+lowest_8cyl = data[data['cyl'] == 8].sort_values(by='mpg', ascending=False).head(3)
+print("\nTri automobila s 8 cilindara s najmanjom potrošnjom:")
+print(lowest_8cyl[['car', 'mpg']])
 
-new_cars4 = mtcars[(mtcars.cyl==4) & ((mtcars.wt > 2.000) & (mtcars.wt <2.200))]
-print(new_cars4['mpg'].mean())
+# 3. Srednja potrošnja automobila sa 6 cilindara
+avg_mpg_6cyl = data[data['cyl'] == 6]['mpg'].mean()
+print(f"\nSrednja potrošnja automobila sa 6 cilindara: {avg_mpg_6cyl:.2f} mpg")
 
-rucni = 0
-autom = 0
+# 4. Srednja potrošnja automobila s 4 cilindra mase između 2000 i 2200 lbs
+filtered_4cyl = data[(data['cyl'] == 4) & (data['wt'] * 1000 >= 2000) & (data['wt'] * 1000 <= 2200)]
+avg_mpg_4cyl = filtered_4cyl['mpg'].mean()
+print(f"\nSrednja potrošnja automobila s 4 cilindra mase između 2000 i 2200 lbs: {avg_mpg_4cyl:.2f} mpg")
 
-print("Rucni: ", rucni)
-print("Automatik: ", autom)
+# 5. Broj automobila s ručnim (am=1) i automatskim (am=0) mjenjačem
+transmission_counts = data['am'].value_counts()
+print(f"\nAutomatski: {transmission_counts[0]}, Ručni: {transmission_counts[1]}")
 
+# 6. Automobili s automatskim mjenjačem i snagom preko 100 konjskih snaga
+auto_over_100hp = data[(data['am'] == 0) & (data['hp'] > 100)]
+print(f"\nBroj automobila s automatskim mjenjačem i snagom preko 100 KS: {len(auto_over_100hp)}")
+
+# 7. Masa svakog automobila u lbs
+print("\nMasa svakog automobila u lbs:")
+print(data[['car', 'wt']])
 
